751a03f00e36c4bb6ecf016c0981c1a989a66da9
Rahul Arya <rahularya50@gmail.com>
2016-07-12 02:32:55 +0800

    Updated repo

---
+++ Lionel3/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Lionel3
+//
+//  Created by Rahul Arya on 12/7/2016.
+//  Copyright © 2016 No Empty Promises. All rights reserved.
+//
+
+#import "IASKAppSettingsViewController.h"
+
+@interface SettingsTableViewController : IASKAppSettingsViewController
+
+@end
---
+++ Lionel3/SettingsTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  SettingsTableViewController.m
+//  Lionel3
+//
+//  Created by Rahul Arya on 12/7/2016.
+//  Copyright © 2016 No Empty Promises. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+
+@interface SettingsTableViewController ()
+
+@end
+
+@implementation SettingsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
